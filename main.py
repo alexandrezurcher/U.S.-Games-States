3c372a7e006793fb34107be0016149846b77ab93
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-12 07:34:26 +0100

    getting user input

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-#with this method we can get the cordinate on the turtle screen
-def get_mouse_click_coor(x, y):
-    print(x, y)
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
 
+#with this method we can get the cordinate on the turtle screen
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the State", prompt="Choose a U.S. State Name")
+print(answer_state)
